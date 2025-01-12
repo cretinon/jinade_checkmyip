46fa7b8739551b42be1226adee69248161d1142e
cretinon <jacques@cretinon.fr>
2025-01-12 05:03:15 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#curl -H 'Authorization: Basic xxxxxxxxxx' "https://nic.ChangeIP.com/nic/update?&hostname=checkmyip.ddns.info&set=1"
+
 while [ 1 ]; do
   date
   curl -A "checkmyip.ddns.info SET" -u $CHECKMYIP_USER:$CHECKMYIP_PASS "https://nic.changeip.com/nic/update?cmd=update&set=1"
